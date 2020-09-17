9e977b6c31df6b1df97b54de56d4f340f539755a
Gauti <gudmundurgj20@ru.is>
2020-09-17 10:33:40 +0000

    def direction

--- tile_traveler.py
+++ tile_traveler.py
@@ -9,14 +9,27 @@ def directions(column, row):
     """
     returns where the player can go
     """
+    valid_direction = ""
+    if  possition == "1,1" or possition == "2,1" or possition == "2,1":
+        valid_direction = "(n)orth"
+    elif possition == "1,2" or possition == "3,2":
+        valid_direction = "(n)orth, (s)outh"
+    elif possition == "1,3":
+        valid_direction = "(s)outh, (e)ast"
+    elif possition == "2,3":
+        valid_direction = "(e)ast, (w)est"
+    elif possition == "3,3" or possition == "2,2":
+        valid_direction = "(w)est, (s)outh"
+    else:
+        #victory
+    return valid_direction
 def move(direction):
     """ 
     moves the player and returns new position 
     """
 # Main program
 
-pos_col = 1
-pos_row = 1
+possition = "1,1"
 
 while pos_col != 3 and pos_row != 1:
     where_to_go = str(input())
