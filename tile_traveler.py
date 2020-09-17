8f3560d7d32e2741dd6972f0a97c4bafc9d49e27
gulli20 <gunnlaugur20@ru.is>
2020-09-17 10:12:16 +0000

    Skeleton done

--- tile_traveler.py
+++ tile_traveler.py
@@ -4,3 +4,19 @@
     # if sentence to retrun the right moves (print)
 # func move(direction) and call func moves with new position (player pos_row pos_col)
 # when player enters pos_col = 3 and pos_row= 1
+
+def directions(column, row):
+    """
+    returns where the player can go
+    """
+def move(direction):
+    """ 
+    moves the player and returns new position 
+    """
+# Main program
+
+pos_col = 1
+pos_row = 1
+
+while pos_col != 3 and pos_row != 1:
+    where_to_go = str(input())
